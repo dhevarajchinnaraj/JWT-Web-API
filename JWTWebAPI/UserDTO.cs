ca53fa9fa0a03b376cd572dd39765171f7db77e1
dhevarajchinnaraj <dhevarajchinnaraj@gmail.com>
2023-07-08 20:33:46 +0530

    Add project files.

---
+++ JWTWebAPI/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTWebAPI
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
